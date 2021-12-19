45b168ffaae7eddb16ac1a22a2f63d5ca27b505c
LuisCruz <luis1016@live.com.mx>
2021-12-19 03:39:52 -0600

    Cubic URL

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
+const uri = 'https://cubicapp.github.io/cubicappweb/';
+
 ReactDOM.render(
   <Auth0Provider
-  domain={domain}
-  clientId={clientId}
-  redirectUri={window.location.origin}>
+    domain={domain}
+    clientId={clientId}
+    redirectUri={uri}>
     <App />
   </Auth0Provider>,
   document.getElementById('root')
