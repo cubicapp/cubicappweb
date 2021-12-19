4f51d4bbc77303ae5c8fac5586abf6b4940e2df0
LuisCruz <luis1016@live.com.mx>
2021-12-19 03:11:42 -0600

    Auth0 changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginButton from './components/LoginButton';
+import { LogoutButton } from './components/LogoutButton';
+import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if(isLoading) return <div>Loading...</div>
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           !Welcome to Cubicappweb!
         </p>
+        <>
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
+        </>
       </header>
     </div>
   );
